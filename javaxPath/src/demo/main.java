d24efaa7a3414301e77fc3b43e042da8a1071d44
spatel230 <41339115+spatel230@users.noreply.github.com>
2020-04-15 11:14:02 -0400

    Update main.java

--- javaxPath/src/demo/main.java
+++ javaxPath/src/demo/main.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 import xpath.*;
 public class main {
 	public static void main(String[]args) {
-		/*Scanner sc=new Scanner(System.in);
+		Scanner sc=new Scanner(System.in);
 		System.out.print("What is the title of the use case");
-		String x=sc.next();*/
-		CapecXPath.display();
+		String x=sc.next();
+		CapecXPath.display(x);
 	}
 
 }
